a650d47e87bea00d1368e462d9af1b09ef00f245
kyle1668 <kyleobrien01@comcast.net>
2018-02-17 17:27:06 -0800

    ported code

--- src/App.js
+++ src/App.js
@@ -2,20 +2,75 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
+        < Jumbotron title="S.E.F." subTitle="Social Engineer Firewall"/>
+        
+        <div class="container">
+          <TextLine />
+          <TextLine />
+          <TextLine />
+        </div>
       </div>
     );
   }
 }
 
-export default App;
+export class TextLine extends React.Component {
+  render() {
+    return (
+      <div class="text-line">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="card">
+              <div class="card-block">
+                <br/>
+                <h4 class="card-title">Card title</h4>
+                <h6 class="card-subtitle mb-2 text-muted">{"Card subtitle: " + getDate()}</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <br/>              
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class Jumbotron extends React.Component {
+  render() {
+    return (
+      <div class="container head-section">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="jumbotron">
+              <h1>{this.props.title}</h1>
+              <p>{this.props.subTitle}</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button" onClick={test}><i class="fas fa-microphone"></i></a></p>    
+            </div>  
+          </div>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+function test() {
+  console.log("printtt");
+}
+
+function getDate() {
+  var date = new Date().toString().split(" ");
+  var shortDate = "";
+
+  for(var i = 0; i < 5; i++) {
+    shortDate += " " + date[i]
+  }
+
+  return shortDate;
+}
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import {App, Jumbotron} from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = document.getElementById('root');
+
+ReactDOM.render(<App />, root)
 registerServiceWorker();
